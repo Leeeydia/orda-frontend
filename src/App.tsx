63afb4c4036b60aacf9fccef8b34a551b4d2d1a5
leeeydia <leeeydia@gmail.com>
2026-03-09 10:52:51 +0900

    add: 프론트엔드 개발환경 세팅

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">ORDA 🏔️</h1>
+    </div>
+  );
+};
+
+export default App;
