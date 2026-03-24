a283df0f4faa06a90afbb10da117230a50904ac6
leeeydia <leeeydia@gmail.com>
2026-03-24 12:20:35 +0900

    feat: HikingPage에 등산 API 연동

--- src/pages/HikingPage.tsx
+++ src/pages/HikingPage.tsx
@@ -1,9 +1,30 @@
 import GpsTrackingMap from "@/features/gps/components/GpsTrackingMap";
+import { useHiking } from "@/features/hiking/hooks/useHiking";
 
 export default function HikingPage() {
+  const {
+    geoJson,
+    currentPos,
+    trail,
+    isTracking,
+    isLoading,
+    error,
+    start,
+    stop
+  } = useHiking();
+
   return (
     <div style={{ width: "100vw", height: "100dvh", position: "relative" }}>
-      <GpsTrackingMap />
+      <GpsTrackingMap
+        geoJson={geoJson}
+        currentPos={currentPos}
+        trail={trail}
+        isTracking={isTracking}
+        isLoading={isLoading}
+        error={error}
+        onStart={start}
+        onStop={stop}
+      />
     </div>
   );
 }
