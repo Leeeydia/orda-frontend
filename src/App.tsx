32062b6a63369048837233c2f509a93614e876ea
yoonjongmin <yxh750501@gmail.com>
2026-03-25 12:49:53 +0900

    feat: 마이페이지 라우터 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MapTestPage from "./pages/MapTestPage";
 import HikingPage from "./pages/HikingPage";
+import MyPage from "./pages/MyPage";
 
 export default function App() {
   return (
@@ -8,6 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MapTestPage />} />
         <Route path="/hiking" element={<HikingPage />} />
+        <Route path="/mypage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
