0eec39c12f6d94b374388cc3deed034da3dded9b
leeeydia <leeeydia@gmail.com>
2026-03-26 16:29:19 +0900

    feat: /login 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MapTestPage from "./pages/MapTestPage";
 import HikingPage from "./pages/HikingPage";
 import SignupPage from "./pages/auth/SignupPage";
+import LoginPage from "./pages/auth/LoginPage";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<MapTestPage />} />
         <Route path="/hiking" element={<HikingPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
       </Routes>
     </BrowserRouter>
   );
