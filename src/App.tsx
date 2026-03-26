3b97e81a00cf39dca0ddbb6f4cbda15797160878
May <may.code.28@gmail.com>
2026-03-26 17:20:07 +0900

    feat: 지도 중심 세션 상세 페이지 레이아웃 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MapTestPage from "./pages/MapTestPage";
-import HikingPage from "./pages/HikingPage";
+import HikingPage from "./pages/hiking/HikingPage";
+import HikingSessionDetailPage from "./pages/hiking/HikingSessionDetailPage";
 import SignupPage from "./pages/auth/SignupPage";
 import LoginPage from "./pages/auth/LoginPage";
+
 
 export default function App() {
   return (
@@ -10,6 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<MapTestPage />} />
         <Route path="/hiking" element={<HikingPage />} />
+        <Route path="/hiking/sessions/:sessionId" element={<HikingSessionDetailPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
       </Routes>
