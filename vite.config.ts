28e3e1df9281ea9dc815c3eb1aecc0cc34d7612c
leeeydia <leeeydia@gmail.com>
2026-03-30 14:40:52 +0900

    chore: vite-plugin-svgr 플러그인 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import svgr from "vite-plugin-svgr";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   server: {
-  host: true,
-  allowedHosts: ["untrivial-uncausatively-shaun.ngrok-free.dev"],
-  proxy: {
-    '/api': 'http://localhost:8080'  // 이거 추가
-  }
-},
+    host: true,
+    allowedHosts: ["untrivial-uncausatively-shaun.ngrok-free.dev"],
+    proxy: {
+      "/api": "http://localhost:8080"
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src")
